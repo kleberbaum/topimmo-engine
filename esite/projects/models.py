ee6edec7186ee9bc4d45da4aa1b9b7c5de66821e
Simon <simon.prast.sp@gmail.com>
2020-10-11 22:43:52 +0200

    Alter Projects Page and add Flats Page
    Edit the Projects Page, which is now available to serve for buildings.
    Add the Flats Page, which is now available to serve for flats. Flats Pages can be linked to Projects Pages (1:n).

--- esite/projects/models.py
+++ esite/projects/models.py
@@ -16,13 +16,14 @@ from modelcluster.fields import ParentalKey
 from esite.colorfield.fields import ColorField, ColorAlphaField
 from esite.colorfield.blocks import ColorBlock, ColorAlphaBlock, GradientColorBlock
 
-#from grapple.models import (
+# from grapple.models import (
 #    GraphQLField,
 #    GraphQLString,
 #    GraphQLStreamfield,
-#)
+# )
 
 # Create your homepage related models here.
+
 
 @register_snippet
 class Button(models.Model):
@@ -48,26 +49,48 @@ class Button(models.Model):
         return self.button_title
 
 
-#> Header
+# > Header
 class _H_HeroBlock(blocks.StructBlock):
-    slide_image = ImageChooserBlock(required=True, blank=False, help_text="Großes, hochauflösendes Titelbild")
+    slide_image = ImageChooserBlock(
+        required=True, blank=False, help_text="Großes, hochauflösendes Titelbild")
+
+
+class _S_InfoBlock(blocks.StructBlock):
+    thumbnail_image = ImageChooserBlock(
+        required=True, blank=False, help_text="Großes, hochauflösendes Titelbild")
+    info_text = blocks.RichTextBlock(
+        label='Text', required=False, help_text="Info-Text")
+
 
 class _S_ContentCenter(blocks.StructBlock):
-    content_center_head = blocks.CharBlock(required=False, help_text="Content-Center Header")
-    content_center_lead = blocks.CharBlock(required=False, help_text="Content-Center Untertitel")
-    content_center_text = blocks.RichTextBlock(label='Text', required=False, help_text="Content-Center Text")
-    
+    content_center_head = blocks.CharBlock(
+        required=False, help_text="Content-Center Header")
+    content_center_lead = blocks.CharBlock(
+        required=False, help_text="Content-Center Untertitel")
+    content_center_text = blocks.RichTextBlock(
+        label='Text', required=False, help_text="Content-Center Text")
+
+
 class _S_ContentRight(blocks.StructBlock):
-    content_right_img = ImageChooserBlock(required=False, help_text="Content-Right Titelbild")
-    content_right_head = blocks.CharBlock(required=False, help_text="Content-Right Header")
-    content_right_lead = blocks.CharBlock(required=False, help_text="Content-Right Untertitel")
-    content_right_text = blocks.RichTextBlock(label='Text', required=False, help_text="Content-Right Text")
+    content_right_img = ImageChooserBlock(
+        required=False, help_text="Content-Right Titelbild")
+    content_right_head = blocks.CharBlock(
+        required=False, help_text="Content-Right Header")
+    content_right_lead = blocks.CharBlock(
+        required=False, help_text="Content-Right Untertitel")
+    content_right_text = blocks.RichTextBlock(
+        label='Text', required=False, help_text="Content-Right Text")
+
 
 class _S_ContentLeft(blocks.StructBlock):
-    content_left_img = ImageChooserBlock(required=False, help_text="Content-Left Titelbild")
-    content_left_head = blocks.CharBlock(required=False, help_text="Content-Left Header")
-    content_left_lead = blocks.CharBlock(required=False, help_text="Content-Left Untertitel")
-    content_left_text = blocks.RichTextBlock(label='Text', required=False, help_text="Content-Left Text")
+    content_left_img = ImageChooserBlock(
+        required=False, help_text="Content-Left Titelbild")
+    content_left_head = blocks.CharBlock(
+        required=False, help_text="Content-Left Header")
+    content_left_lead = blocks.CharBlock(
+        required=False, help_text="Content-Left Untertitel")
+    content_left_text = blocks.RichTextBlock(
+        label='Text', required=False, help_text="Content-Left Text")
 
 # class GalleryImage(blocks.StructBlock):
 #     gallery_image = ImageChooserBlock(required=True, blank=False, help_text="Galerie-Bild")
@@ -77,17 +100,35 @@ class _S_ContentLeft(blocks.StructBlock):
 #         ('image', GalleryImage(required=False, icon='fa-info'))
 #     ], required=True, help_text="Bilder-Galerie")
 
-class _G_GalleryBlock(blocks.StructBlock):
-    gallery_image = ImageChooserBlock(required=True, blank=False, help_text="Galerie-Bild")
 
-#> Homepage
+class _G_GalleryBlock(blocks.StructBlock):
+    gallery_image = ImageChooserBlock(
+        required=True, blank=False, help_text="Galerie-Bild")
+
+
+class _P_GroundPlanBlock(blocks.StructBlock):
+    ground_plan = ImageChooserBlock(
+        required=True, blank=False, help_text="Grundriss")
+
+
+class _F_FlatsBlock(blocks.StructBlock):
+    flat = blocks.PageChooserBlock(
+        required=False, help_text="Wohneinheit im Gebäude")
+
+
 class ProjectsPage(Page):
     headers = StreamField([
         ('h_hero', _H_HeroBlock(icon='image')),
         # ('code', blocks.RawHTMLBlock(null=True, blank=True, classname="full", icon='code'))
     ])
 
+    price_min = models.IntegerField(null=True, blank=False)
+    price_max = models.IntegerField(null=True, blank=True)
+    buy_available = models.BooleanField()
+    rent_available = models.BooleanField()
+
     sections = StreamField([
+        ('s_info', _S_InfoBlock(icon='fa-info')),
         ('s_contentcenter', _S_ContentCenter(icon='fa-info')),
         ('s_contentright', _S_ContentRight(icon='fa-info')),
         ('s_contentleft', _S_ContentLeft(icon='fa-info')),
@@ -98,30 +139,102 @@ class ProjectsPage(Page):
         ('g_gallery', _G_GalleryBlock(icon='fa-info'))
     ], null=True, blank=True)
 
+    flats = StreamField([
+        ('f_flats', _F_FlatsBlock(icon='fa-info'))
+    ], null=True, blank=True)
+
     # footers = StreamField([
     # ], null=True, blank=False)
 
     token = models.CharField(null=True, blank=True, max_length=255)
 
-    #graphql_fields = [
+    # graphql_fields = [
     #    GraphQLStreamfield("headers"),
     #    GraphQLStreamfield("sections"),
-    #]
+    # ]
 
     main_content_panels = [
         StreamFieldPanel('headers'),
+        FieldPanel('price_min'),
+        FieldPanel('price_max'),
+        FieldPanel('buy_available'),
+        FieldPanel('rent_available'),
+        StreamFieldPanel('sections'),
+        StreamFieldPanel('gallery'),
+        StreamFieldPanel('flats')
+        # StreamFieldPanel('footers')
+    ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(Page.content_panels + main_content_panels, heading='Main'),
+        ObjectList(Page.promote_panels, heading='Settings',
+                   classname="settings")
+    ])
+
+    preview_modes = []
+
+
+class _S_InfoFlatBlock(blocks.StructBlock):
+    thumbnail_image = ImageChooserBlock(
+        required=True, blank=False, help_text="Großes, hochauflösendes Titelbild")
+    info_text = blocks.RichTextBlock(
+        label='Text', required=False, help_text="Info-Text")
+
+
+class FlatPage(Page):
+    headers = StreamField([
+        ('h_hero', _H_HeroBlock(icon='image')),
+        # ('code', blocks.RawHTMLBlock(null=True, blank=True, classname="full", icon='code'))
+    ])
+
+    price = models.IntegerField(null=True, blank=False)
+    lead = models.CharField(null=True, blank=True, max_length=512)
+    available = models.BooleanField()
+    # ground_plan = ImageChooserBlock(
+    #     required=True, blank=False, help_text="Raumplan")
+
+    sections = StreamField([
+        ('s_contentcenter', _S_ContentCenter(icon='fa-info')),
+        ('s_contentright', _S_ContentRight(icon='fa-info')),
+        ('s_contentleft', _S_ContentLeft(icon='fa-info'))
+    ], null=True, blank=True)
+
+    gallery = StreamField([
+        ('g_gallery', _G_GalleryBlock(icon='fa-info'))
+    ], null=True, blank=True)
+
+    ground_plan = StreamField([
+        ('p_groundplan', _P_GroundPlanBlock(icon='fa-info'))
+    ], null=True, blank=True)
+
+    main_content_panels = [
+        StreamFieldPanel('headers'),
+        FieldPanel('price'),
+        FieldPanel('lead'),
+        FieldPanel('available'),
+        # FieldPanel('ground_plan'),
+        StreamFieldPanel('ground_plan'),
         StreamFieldPanel('sections'),
         StreamFieldPanel('gallery'),
         # StreamFieldPanel('footers')
     ]
 
-    token_panel = [
-        FieldPanel('token')
-    ]
-
     edit_handler = TabbedInterface([
         ObjectList(Page.content_panels + main_content_panels, heading='Main'),
-        ObjectList(Page.promote_panels + token_panel + Page.settings_panels, heading='Settings', classname="settings")
+        ObjectList(Page.promote_panels, heading='Settings',
+                   classname="settings")
     ])
 
     preview_modes = []
+
+
+# > Homepage
+class Folder(Page):
+    # graphql_fields = [
+    #    GraphQLStreamfield("headers"),
+    #    GraphQLStreamfield("sections"),
+    # ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(Page.content_panels, heading='Main'),
+    ])
