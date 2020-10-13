16650a98c1c9f535e41d2e18762252e9655a74c7
Simon <simon.prast.sp@gmail.com>
2020-10-13 02:31:02 +0200

    Add core custom css
    Add custom css for the CMS interface and change the logo.

--- esite/core/static/core/custom.js
+++ esite/core/static/core/custom.js
@@ -1,6 +1,6 @@
 $(function() {
   console.log($(".wagtail-logo-container-inner"))
-    document.querySelector(".wagtail-logo-container-inner").innerHTML='<img class="wagtail-logo wagtail-logo__body" src="/static/core/wolf.svg" alt="">';
+    document.querySelector(".wagtail-logo-container-inner").innerHTML='<img class="wagtail-logo wagtail-logo__body" src="/static/core/im-logo.svg" alt="">';
 });
 
 /**
